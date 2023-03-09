4723457ccb89c54adaa6a3dbecc0ad9f8dc9f40c
jungyu333 <jungyu3826@naver.com>
2023-03-09 15:20:36 +0900

    [feat] signin layout 구현

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,14 @@
 import { DefaultTheme } from 'styled-components';
 
-const colors = {};
+const colors = {
+  ligthgray: '#D9D9D9',
+  white: '#FFFFFF',
+  mainbackground: '#000000',
+  content: '#D6FF5C',
+  gray: '#3E3E47',
+  textlightgray: '#C3C3C6',
+  termsgray: '#9E9EA3',
+};
 
 export type TColorsTypes = typeof colors;
 
