01fe9bb6c7df5fdfa7d3cedf62aeb8332005f4b1
jungyu333 <jungyu3826@naver.com>
2023-03-27 14:48:52 +0900

    [feat] 오늘의 카드 수정 버튼 컴포넌트 layout 구현

---
+++ src/components/Home/TodayCardEditButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardHeaderEdit = styled.li`
+  background-color: ${({ theme }) => theme.colors.gray7};
+  color: ${({ theme }) => theme.colors.gray1};
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius50};
+  width: 49px;
+  height: 30px;
+
+  & button {
+    width: 100%;
+    height: 100%;
+    font-size: 14px;
+    font-weight: 500;
+    padding: 6px 12px;
+  }
+`;
+
+function TodayCardEditButton() {
+  return (
+    <CardHeaderEdit>
+      <button>수정</button>
+    </CardHeaderEdit>
+  );
+}
+
+export default TodayCardEditButton;
