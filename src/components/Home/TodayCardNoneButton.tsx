72206808e2f7d43552d24a10eb1ad0eb134f225c
jungyu333 <jungyu3826@naver.com>
2023-03-27 14:55:21 +0900

    [feat] 인기 옷장이 없을때의 버튼 layout 구현

---
+++ src/components/Home/TodayCardNoneButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardItemsNone = styled.div`
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius20};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.gray5};
+  color: ${({ theme }) => theme.colors.gray8};
+  height: 91px;
+  margin: 24px 16px 0 16px;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+function TodayCardNoneButton() {
+  return <CardItemsNone>오늘의 인기 옷장이 되어보세요.</CardItemsNone>;
+}
+
+export default TodayCardNoneButton;
