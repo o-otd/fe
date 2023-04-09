22be2affcd558adf1e9dfa3088685d3955babbc0
jungyu333 <jungyu3826@naver.com>
2023-04-09 19:22:28 +0900

    [feat] 이메일 회원가입 input validation 구현

--- src/types/SignIn/index.ts
+++ src/types/SignIn/index.ts
@@ -2,3 +2,8 @@ export interface IEmailSignInInputData {
   email: string;
   password: string;
 }
+
+export interface IEmailSignUpInputData extends IEmailSignInInputData {
+  passwordCheck: string;
+  nickName: string;
+}
