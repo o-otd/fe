7c74878708bd95f15e98d7d48e00c7d0d24ed9c0
jungyu333 <jungyu3826@naver.com>
2023-05-16 16:15:04 +0900

    [feat] confirm comment layout 구현

--- src/page/Confirm.tsx
+++ src/page/Confirm.tsx
@@ -8,7 +8,7 @@ import dummyImage from '../styles/images/dummy-profile.png';
 import confirmImage from '../styles/images/confirm-img-dummy.png';
 import { ReactComponent as RefreshSVG } from '../styles/images/icons/refresh.svg';
 import { ReactComponent as ConfirmVoteSVG } from '../styles/images/icons/confirm-vote-icon.svg';
-
+import ConfirmCommentLinkSVG from '../styles/images/icons/confirm-comments-link.svg';
 const ConfirmUtil = styled.div`
   display: flex;
   justify-content: flex-end;
@@ -193,6 +193,67 @@ const CustomSwiper = styled(Swiper)`
   }
 `;
 
+const ConfirmListItem = styled.li`
+  & + & {
+    margin-top: 24px;
+  }
+`;
+
+const ConfirmComments = styled.div`
+  padding: 22px 16px;
+  margin-top: 4px;
+  background: ${({ theme }) => theme.colors.gray2};
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius30};
+`;
+
+const ConfirmCommentsInfo = styled.h5`
+  font-size: 17px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.white};
+
+  & span {
+    font-size: 14px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.gray5};
+    margin-left: 7px;
+  }
+`;
+
+const ConfirmCommentsList = styled.ul`
+  margin-top: 11px;
+`;
+
+const ConfirmCommentsItem = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+const ConfirmCommentsProfile = styled.div`
+  width: 24px;
+  height: 24px;
+  background-color: ${({ theme }) => theme.colors.main};
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius50};
+  overflow: hidden;
+`;
+
+const ConfirmCommentsContent = styled.p`
+  width: calc(100% - 40px);
+  position: relative;
+  margin-left: 16px;
+  font-size: 14px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.gray8};
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    background: url(${ConfirmCommentLinkSVG}) no-repeat center / cover;
+    right: 0;
+  }
+`;
+
 function Confirm() {
   return (
     <>
@@ -206,7 +267,7 @@ function Confirm() {
 
         <section>
           <ul>
-            <li>
+            <ConfirmListItem>
               <ConfirmCard>
                 <ConfirmCardInfo>
                   <ConfirmCardProfile>
@@ -253,7 +314,84 @@ function Confirm() {
                   ))}
                 </CustomSwiper>
               </ConfirmSlider>
-            </li>
+
+              <ConfirmComments>
+                <ConfirmCommentsInfo>
+                  댓글 <span>72</span>
+                </ConfirmCommentsInfo>
+                <ConfirmCommentsList>
+                  <ConfirmCommentsItem>
+                    <ConfirmCommentsProfile />
+                    <ConfirmCommentsContent>
+                      패턴이 너무 튀어요.
+                    </ConfirmCommentsContent>
+                  </ConfirmCommentsItem>
+                </ConfirmCommentsList>
+              </ConfirmComments>
+            </ConfirmListItem>
+
+            <ConfirmListItem>
+              <ConfirmCard>
+                <ConfirmCardInfo>
+                  <ConfirmCardProfile>
+                    <CoverImage src={dummyImage} />
+                  </ConfirmCardProfile>
+                  <div>
+                    <ConfirmCardNickName>CNVXCX</ConfirmCardNickName>
+                    <ConfirmCardDate>
+                      7일 남음 <span>2022.11.27 ~ 2022.12.04</span>
+                    </ConfirmCardDate>
+                  </div>
+                </ConfirmCardInfo>
+                <ConfirmCardContent>
+                  이번 주말에 데이트 나가는데 어떤지 봐주세요. 키 178, 몸무게
+                  72kg 평소에 무난하게 입습니다. 급해요 꼭 투표부탁. 댓글로 다른
+                  스타일 추천도 받습니다! 부탁!!!!!!!!!!
+                </ConfirmCardContent>
+              </ConfirmCard>
+
+              <ConfirmVote>
+                <ConfirmVoteInfo>
+                  <ConfirmVoteSVG />
+                  <h5>PICK</h5>
+                  <ConfirmVoteData>
+                    7일 남음
+                    <span>2022.11.27 ~ 2022.12.04</span>
+                  </ConfirmVoteData>
+                </ConfirmVoteInfo>
+                <ConfirmVoteList type="button">입고 나가요</ConfirmVoteList>
+                <ConfirmVoteList type="button">다시 골라요</ConfirmVoteList>
+
+                <ConfirmVoteBtns>
+                  <ConfirmVoteSubmit type="submit">투표하기</ConfirmVoteSubmit>
+                  <ConfirmVoteResult type="button">결과 확인</ConfirmVoteResult>
+                </ConfirmVoteBtns>
+              </ConfirmVote>
+
+              <ConfirmSlider>
+                <CustomSwiper pagination modules={[Pagination]}>
+                  {[1, 2, 3, 4, 5].map((item) => (
+                    <ConfirmSliderItem key={item}>
+                      <CoverImage src={confirmImage} alt="thumbnail" />
+                    </ConfirmSliderItem>
+                  ))}
+                </CustomSwiper>
+              </ConfirmSlider>
+
+              <ConfirmComments>
+                <ConfirmCommentsInfo>
+                  댓글 <span>72</span>
+                </ConfirmCommentsInfo>
+                <ConfirmCommentsList>
+                  <ConfirmCommentsItem>
+                    <ConfirmCommentsProfile />
+                    <ConfirmCommentsContent>
+                      패턴이 너무 튀어요.
+                    </ConfirmCommentsContent>
+                  </ConfirmCommentsItem>
+                </ConfirmCommentsList>
+              </ConfirmComments>
+            </ConfirmListItem>
           </ul>
         </section>
       </main>
