04a8eb5f94ee6ab64940d1771f2dea2d9c162a87
jungyu333 <jungyu3826@naver.com>
2023-05-24 17:59:54 +0900

    [feat] comment detail comment input layout 구현

--- src/page/confirm/CommentDetail.tsx
+++ src/page/confirm/CommentDetail.tsx
@@ -13,6 +13,16 @@ function CommentDetail() {
           댓글 <span>72</span>
         </CommentTopTitle>
       </CommentTop>
+
+      <CommentForm>
+        <form>
+          <CommentTextArea placeholder="댓글을 입력하세요" />
+          <CommentInputUtil>
+            <span>0</span>/600
+            <CommentFormSubmit>댓글 남기기</CommentFormSubmit>
+          </CommentInputUtil>
+        </form>
+      </CommentForm>
     </main>
   );
 }
@@ -43,4 +53,53 @@ const CommentTopTitle = styled.div`
     font-weight: 400;
     color: ${({ theme }) => theme.colors.gray5};
   }
+`;
+
+const CommentForm = styled.section`
+  margin-top: 4px;
+  padding: 22px 16px 20px 16px;
+  background-color: ${({ theme }) => theme.colors.gray2};
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius20};
+`;
+
+const CommentTextArea = styled.textarea`
+  display: block;
+  padding: 16px;
+  width: 100%;
+  height: 103px;
+  resize: none;
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius10};
+  background-color: ${({ theme }) => theme.colors.gray2};
+  border: 1px solid ${({ theme }) => theme.colors.gray5};
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.gray8};
+  font-size: 14px;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.gray5};
+  }
+`;
+
+const CommentInputUtil = styled.div`
+  margin-top: 11px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 14px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.gray5};
+`;
+
+const CommentFormSubmit = styled.button`
+  margin-left: 23px;
+  background-color: ${({ theme }) => theme.colors.gray8};
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius10};
+  font-weight: 600;
+  font-size: 14px;
+  padding: 10px 12px;
+  color: ${({ theme }) => theme.colors.gray2};
+
+  /* disabled
+  background-color: var(--gray5);  */
 `;
