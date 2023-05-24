7b30fd9a1e3dafa2b65328334b092904737e3db1
jungyu333 <jungyu3826@naver.com>
2023-05-24 17:35:45 +0900

    [feat] comment detail page header layout 구현

--- src/page/confirm/CommentDetail.tsx
+++ src/page/confirm/CommentDetail.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as ConfirmCommentCloseSVG } from '../../styles/images/icons/confirm-comments-close.svg';
 
 function CommentDetail() {
-  return <div>CommentDetail</div>;
+  return (
+    <main>
+      <CommentTop>
+        <CommentTopClose>
+          <ConfirmCommentCloseSVG />
+        </CommentTopClose>
+        <CommentTopTitle>
+          댓글 <span>72</span>
+        </CommentTopTitle>
+      </CommentTop>
+    </main>
+  );
 }
 
 export default CommentDetail;
+
+const CommentTop = styled.section`
+  display: flex;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.gray3};
+  height: 55px;
+  border-radius: ${({ theme }) => theme.borderRadius.headerRadius30};
+  padding: 0 16px;
+`;
+
+const CommentTopClose = styled.button`
+  height: 24px;
+`;
+
+const CommentTopTitle = styled.div`
+  margin-left: 12px;
+  font-size: 17px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.gray9};
+
+  & span {
+    margin-left: 6px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.gray5};
+  }
+`;
