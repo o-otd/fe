f8167bbeeb8d9abf087ecc5280bde5228e26208c
jungyu333 <jungyu3826@naver.com>
2023-05-25 17:43:38 +0900

    [feat] comment layout 구현

--- src/page/confirm/CommentDetail.tsx
+++ src/page/confirm/CommentDetail.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ReactComponent as ConfirmCommentCloseSVG } from '../../styles/images/icons/confirm-comments-close.svg';
+import { ReactComponent as ConfirmCommentLikeSVG } from '../../styles/images/icons/likes.svg';
 
 function CommentDetail() {
   return (
@@ -23,6 +24,28 @@ function CommentDetail() {
           </CommentInputUtil>
         </form>
       </CommentForm>
+
+      <CommentList>
+        <ul>
+          <CommentItem>
+            <CommentListProfile />
+            <CommentListInfo>
+              <CommentListNickName>닉네임</CommentListNickName>
+              <CommentListContent>패턴이 너무 튀어요.</CommentListContent>
+
+              <CommentListUtil>
+                <CommentListComments>
+                  댓글 <span>13</span>
+                </CommentListComments>
+                <CommentListLikes>
+                  <ConfirmCommentLikeSVG />
+                  <span>124</span>
+                </CommentListLikes>
+              </CommentListUtil>
+            </CommentListInfo>
+          </CommentItem>
+        </ul>
+      </CommentList>
     </main>
   );
 }
@@ -102,4 +125,78 @@ const CommentFormSubmit = styled.button`
 
   /* disabled
   background-color: var(--gray5);  */
+`;
+
+const CommentList = styled.section`
+  padding: 22px 16px;
+  margin-top: 4px;
+  background-color: ${({ theme }) => theme.colors.gray2};
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius20};
+`;
+
+const CommentItem = styled.li`
+  display: flex;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray5};
+  padding-bottom: 20px;
+
+  & + & {
+    margin-top: 16px;
+  }
+`;
+
+const CommentListProfile = styled.div`
+  width: 24px;
+  height: 24px;
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius50};
+  background-color: ${({ theme }) => theme.colors.gray8};
+`;
+
+const CommentListInfo = styled.div`
+  width: calc(100% - 40px);
+  font-size: 14px;
+  margin-left: 16px;
+`;
+
+const CommentListNickName = styled.div`
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const CommentListContent = styled.p`
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.gray8};
+  margin-top: 4px;
+`;
+
+const CommentListUtil = styled.div`
+  font-size: 13px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  margin-top: 12px;
+  /* 댓글 눌렀을 때 8px 추가 해야함. */
+  padding-bottom: 8px;
+`;
+
+const CommentListComments = styled.div`
+  color: ${({ theme }) => theme.colors.white};
+
+  & span {
+    margin-left: 4px;
+    color: ${({ theme }) => theme.colors.gray7};
+  }
+`;
+
+const CommentListLikes = styled.button`
+  display: flex;
+  align-items: center;
+  margin-left: 16px;
+
+  & svg {
+    fill: ${({ theme }) => theme.colors.gray5};
+  }
+
+  & span {
+    margin-left: 4px;
+  }
 `;
