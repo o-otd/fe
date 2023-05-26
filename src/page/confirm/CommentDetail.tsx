73bd0606a9d65233edb71a5472b4bfc5fa8abd20
jungyu333 <jungyu3826@naver.com>
2023-05-26 20:39:07 +0900

    [refactor] comment detail page header component 분리 & close button 기능 추가

--- src/page/confirm/CommentDetail.tsx
+++ src/page/confirm/CommentDetail.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as ConfirmCommentCloseSVG } from '../../styles/images/icons/confirm-comments-close.svg';
 import { ReactComponent as ConfirmCommentLikeSVG } from '../../styles/images/icons/likes.svg';
 import { ReactComponent as MoreSVG } from '../../styles/images/icons/more.svg';
+import CommentDetailHeader from 'components/CommentDetail/CommentDetailHeader';
 
 function CommentDetail() {
   return (
     <main>
-      <CommentTop>
-        <CommentTopClose>
-          <ConfirmCommentCloseSVG />
-        </CommentTopClose>
-        <CommentTopTitle>
-          댓글 <span>72</span>
-        </CommentTopTitle>
-      </CommentTop>
+      <CommentDetailHeader />
 
       <CommentForm>
         <form>
@@ -81,32 +74,6 @@ function CommentDetail() {
 }
 
 export default CommentDetail;
-
-const CommentTop = styled.section`
-  display: flex;
-  align-items: center;
-  background-color: ${({ theme }) => theme.colors.gray3};
-  height: 55px;
-  border-radius: ${({ theme }) => theme.borderRadius.headerRadius30};
-  padding: 0 16px;
-`;
-
-const CommentTopClose = styled.button`
-  height: 24px;
-`;
-
-const CommentTopTitle = styled.div`
-  margin-left: 12px;
-  font-size: 17px;
-  font-weight: 700;
-  color: ${({ theme }) => theme.colors.gray9};
-
-  & span {
-    margin-left: 6px;
-    font-weight: 400;
-    color: ${({ theme }) => theme.colors.gray5};
-  }
-`;
 
 const CommentForm = styled.section`
   margin-top: 4px;
