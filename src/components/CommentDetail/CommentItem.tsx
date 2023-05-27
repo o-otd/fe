fff6e41cb9bb4163c976fdd154b80e035ad49b48
jungyu333 <jungyu3826@naver.com>
2023-05-27 14:22:34 +0900

    [refactor] comment detail page comments item component 분리

--- src/components/CommentDetail/CommentItem.tsx
+++ src/components/CommentDetail/CommentItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as MoreSVG } from '../../styles/images/icons/more.svg';
 import { ReactComponent as ConfirmCommentLikeSVG } from '../../styles/images/icons/likes.svg';
+import CommentsItem from './CommentsItem';
 
 function CommentItem() {
   return (
@@ -23,21 +23,9 @@ function CommentItem() {
 
         <CommentsList>
           <ul>
-            <Commentsitem>
-              <CommentsItemWidth>
-                <CommentsListInfo>
-                  <CommentsListTitle>XCVXVV</CommentsListTitle>
-                  <CommentsListDate>2022.11.27. 14:27</CommentsListDate>
-                </CommentsListInfo>
-                <CommentsListContents>
-                  공감합니다...공감합니다...공감합니다...공감합니다...공감합니다...공감합니다...
-                </CommentsListContents>
-              </CommentsItemWidth>
-
-              <CommentsListMore>
-                <MoreSVG />
-              </CommentsListMore>
-            </Commentsitem>
+            {[1, 2, 3, 4, 5].map((comments) => (
+              <CommentsItem key={comments} />
+            ))}
           </ul>
 
           <CommentsForm>
@@ -133,49 +121,6 @@ const CommentListDate = styled.div`
 const CommentsList = styled.div`
   padding-top: 8px;
   border-top: 1px solid ${({ theme }) => theme.colors.gray5};
-`;
-
-const Commentsitem = styled.li`
-  position: relative;
-
-  & + & {
-    margin-top: 12px;
-  }
-`;
-
-const CommentsItemWidth = styled.div`
-  width: calc(100% - 24px);
-`;
-
-const CommentsListInfo = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const CommentsListTitle = styled.div`
-  font-size: 14px;
-  font-weight: 500;
-  color: ${({ theme }) => theme.colors.white};
-`;
-
-const CommentsListDate = styled.div`
-  font-size: 12px;
-  font-weight: 400;
-  color: ${({ theme }) => theme.colors.gray6};
-  margin-left: 8px;
-`;
-
-const CommentsListContents = styled.p`
-  margin-top: 4px;
-  font-size: 14px;
-  color: ${({ theme }) => theme.colors.gray8};
-  font-weight: 400;
-`;
-
-const CommentsListMore = styled.button`
-  position: absolute;
-  right: 0;
-  top: 0;
 `;
 
 const CommentsForm = styled.form`
