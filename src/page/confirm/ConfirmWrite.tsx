b111916443f49e6a8314ca9ad96a80dd0a8f5e2d
jungyu333 <jungyu3826@naver.com>
2023-05-28 16:21:42 +0900

    [feat] confirm write page vote layout 구현

--- src/page/confirm/ConfirmWrite.tsx
+++ src/page/confirm/ConfirmWrite.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ReactComponent as ConfirmWriteCloseSVG } from '../../styles/images/icons/confirm-comments-close.svg';
 import styled from 'styled-components';
+import { ReactComponent as ConfirmVoteIconSVG } from '../../styles/images/icons/confirm-vote-icon.svg';
 import ConfirmWriteArrowBottomSVG from '../../styles/images/icons/confirm-write-arrow-bottom.svg';
 import { ReactComponent as CalendarSVG } from '../../styles/images/icons/calendar.svg';
 
@@ -32,6 +33,27 @@ function ConfirmWrite() {
         </WriteFormHeader>
         <WriteFormTextArea placeholder="텍스트를 입력하세요"></WriteFormTextArea>
       </WriteForm>
+
+      <WriteVote>
+        <WriteVoteHeader>
+          <WriteVoteTitle>
+            <ConfirmVoteIconSVG />
+            <div>PICK</div>
+          </WriteVoteTitle>
+          <WriteVoteDate>
+            <WriteVoteDateRemain>7일 남음</WriteVoteDateRemain>
+            <WriteVoteDatePeriod>2022.11.27 ~ 2022.12.04</WriteVoteDatePeriod>
+          </WriteVoteDate>
+        </WriteVoteHeader>
+        <div>
+          <WriteVoteInput>
+            <input type="text" placeholder="선택 1" />
+          </WriteVoteInput>
+          <WriteVoteInput>
+            <input type="text" placeholder="선택 2" />
+          </WriteVoteInput>
+        </div>
+      </WriteVote>
     </main>
   );
 }
@@ -144,4 +166,74 @@ const WriteFormTextArea = styled.textarea`
   resize: none;
   font-size: 14px;
   font-weight: 400;
+`;
+
+const WriteVote = styled.section`
+  padding: 22px 16px 20px 16px;
+  background-color: ${({ theme }) => theme.colors.gray2};
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius20};
+  margin-top: 4px;
+`;
+
+const WriteVoteHeader = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const WriteVoteTitle = styled.div`
+  display: flex;
+  align-items: center;
+
+  & div {
+    font-size: 17px;
+    font-weight: 700;
+    color: ${({ theme }) => theme.colors.gray8};
+    margin-left: 4px;
+  }
+`;
+
+const WriteVoteDate = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 12px;
+`;
+
+const WriteVoteDateRemain = styled.div`
+  font-size: 13px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.gray9};
+  margin-right: 4px;
+`;
+
+const WriteVoteDatePeriod = styled.div`
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.gray6};
+`;
+
+const WriteVoteInput = styled.div`
+  width: 100%;
+  height: 44px;
+  background-color: ${({ theme }) => theme.colors.gray3};
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius10};
+  margin-top: 12px;
+
+  & input {
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+    padding: 13px 16px;
+    border-radius: ${({ theme }) => theme.borderRadius.borderRadius10};
+    font-size: 14px;
+    font-weight: 700;
+
+    &:focus {
+      border: 1px solid ${({ theme }) => theme.colors.main};
+      outline: none;
+    }
+  }
+
+  & + & {
+    margin-top: 4px;
+  }
 `;
