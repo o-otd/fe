7c55ab619b444c97b78490332753cefdfbd62adb
jungyu333 <jungyu3826@naver.com>
2023-06-02 20:42:10 +0900

    [refactor] confirm write text input component 분리

--- src/page/confirm/ConfirmWrite.tsx
+++ src/page/confirm/ConfirmWrite.tsx
@@ -5,32 +5,16 @@ import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { ReactComponent as ConfirmWriteSliderSVG } from '../../styles/images/icons/confirm-write__slider.svg';
 import { ReactComponent as ConfirmVoteIconSVG } from '../../styles/images/icons/confirm-vote-icon.svg';
-import ConfirmWriteArrowBottomSVG from '../../styles/images/icons/confirm-write-arrow-bottom.svg';
-import { ReactComponent as CalendarSVG } from '../../styles/images/icons/calendar.svg';
+
 import ConfirmWriteHeader from 'components/Home/Confirm/ConfirmWriteHeader';
+import ConfirmWriteTextInput from 'components/Home/Confirm/ConfirmWriteTextInput';
 
 function ConfirmWrite() {
   return (
     <form>
       <ConfirmWriteHeader />
 
-      <WriteForm>
-        <WriteFormHeader>
-          <WriteFormProfile />
-          <WriteFormInfo>
-            <WriteFormNickName>CNVXCX</WriteFormNickName>
-            <WriteFormDate>
-              <CalendarSVG />
-              <WriteFormInput>
-                <input type="text" value="2022.11.27" />
-                <span>~</span>
-                <input type="text" value="2022.12.04" />
-              </WriteFormInput>
-            </WriteFormDate>
-          </WriteFormInfo>
-        </WriteFormHeader>
-        <WriteFormTextArea placeholder="텍스트를 입력하세요"></WriteFormTextArea>
-      </WriteForm>
+      <ConfirmWriteTextInput />
 
       <WriteVote>
         <WriteVoteHeader>
@@ -71,83 +55,6 @@ function ConfirmWrite() {
 }
 
 export default ConfirmWrite;
-
-const WriteForm = styled.section`
-  padding: 22px 16px 20px 16px;
-  margin-top: 4px;
-  background-color: ${({ theme }) => theme.colors.gray2};
-  border-radius: ${({ theme }) => theme.borderRadius.borderRadius20};
-`;
-
-const WriteFormHeader = styled.div`
-  display: flex;
-  padding-bottom: 12px;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.gray5};
-`;
-
-const WriteFormProfile = styled.div`
-  width: 40px;
-  height: 40px;
-  background-color: ${({ theme }) => theme.colors.gray9};
-  border-radius: ${({ theme }) => theme.borderRadius.borderRadius50};
-`;
-
-const WriteFormInfo = styled.div`
-  margin-left: 8px;
-`;
-
-const WriteFormNickName = styled.div`
-  font-size: 17px;
-  font-weight: 600;
-  color: ${({ theme }) => theme.colors.white};
-`;
-
-const WriteFormDate = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 2px;
-
-  & input {
-    width: 86px;
-    height: 24px;
-    color: ${({ theme }) => theme.colors.gray1};
-    border-radius: 4px;
-    font-size: 12px;
-    font-weight: 400;
-    border: 1px solid ${({ theme }) => theme.colors.gray5};
-    color: ${({ theme }) => theme.colors.gray3};
-    padding: 8px 5px 5px 4px;
-    background: url(${ConfirmWriteArrowBottomSVG}) no-repeat right / auto,
-      ${({ theme }) => theme.colors.gray7};
-  }
-
-  & span {
-    display: block;
-    margin: 0 4px;
-    font-size: 12px;
-    font-weight: 400;
-    color: ${({ theme }) => theme.colors.gray6};
-  }
-`;
-
-const WriteFormInput = styled.div`
-  margin-left: 4px;
-  display: flex;
-  align-items: center;
-`;
-
-const WriteFormTextArea = styled.textarea`
-  margin-top: 12px;
-  border: 1px solid ${({ theme }) => theme.colors.gray5};
-  border-radius: ${({ theme }) => theme.borderRadius.borderRadius10};
-  background-color: transparent;
-  width: 100%;
-  height: 92px;
-  padding: 16px;
-  resize: none;
-  font-size: 14px;
-  font-weight: 400;
-`;
 
 const WriteVote = styled.section`
   padding: 22px 16px 20px 16px;
