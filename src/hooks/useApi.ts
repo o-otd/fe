f45b7abdd728268a87def9e93b6d9020394b3f00
jungyu333 <jungyu3826@naver.com>
2023-06-06 15:21:27 +0900

    [feat] confirm list get api 구현

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,17 +1,19 @@
 import { useState, useCallback } from 'react';
 
-export const useApi = <T, U>(apiFunc: (params: T) => Promise<U>) => {
+export const useApi = <T = void, U = void>(
+  apiFunc: T extends void ? () => Promise<U> : (params: T) => Promise<U>,
+) => {
   const [error, setError] = useState<Error | null>(null);
 
   const execute = useCallback(
-    async (params: T) => {
+    async (params?: T) => {
       setError(null);
       try {
-        const response = await apiFunc(params);
-        return response; // API 호출의 결과를 반환합니다.
+        const response = await apiFunc(params as T);
+        return response;
       } catch (err) {
         setError(err as Error);
-        return null; // API 호출이 실패하면 null을 반환합니다.
+        return null;
       }
     },
     [apiFunc],
