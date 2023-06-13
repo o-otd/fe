7c8a0bf9f970065ca343b4ba325a0ae6e13a12bb
jungyu333 <jungyu3826@naver.com>
2023-06-13 21:50:01 +0900

    [feat] confirm vote card 투표 선택 기능 구현

--- src/components/Home/Confirm/ConfirmVoteCard.tsx
+++ src/components/Home/Confirm/ConfirmVoteCard.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { ReactComponent as ConfirmVoteSVG } from '@svg/confirm-vote-icon.svg';
+import { ReactComponent as ConfirmCheckSVG } from '@svg/check.svg';
 
 function ConfirmVoteCard() {
+  const [pickValue, setPickValue] = useState<string | null>();
+  const onClickPick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const eventTarget = event.target as HTMLInputElement;
+    setPickValue(eventTarget.value);
+  };
+
   return (
     <ConfirmVote>
       <ConfirmVoteInfo>
@@ -13,11 +20,29 @@ function ConfirmVoteCard() {
           <span>2022.11.27 ~ 2022.12.04</span>
         </ConfirmVoteData>
       </ConfirmVoteInfo>
-      <ConfirmVoteList type="button">입고 나가요</ConfirmVoteList>
-      <ConfirmVoteList type="button">다시 골라요</ConfirmVoteList>
+      <ConfirmVoteList
+        type="button"
+        value={0}
+        onClick={onClickPick}
+        $isActive={pickValue === '0'}
+      >
+        <ConfirmCheckSVG />
+        입고 나가요
+      </ConfirmVoteList>
+      <ConfirmVoteList
+        type="button"
+        value={1}
+        onClick={onClickPick}
+        $isActive={pickValue === '1'}
+      >
+        <ConfirmCheckSVG />
+        다시 골라요
+      </ConfirmVoteList>
 
       <ConfirmVoteBtns>
-        <ConfirmVoteSubmit type="submit">투표하기</ConfirmVoteSubmit>
+        <ConfirmVoteSubmit type="submit" disabled={!Boolean(pickValue)}>
+          투표하기
+        </ConfirmVoteSubmit>
         <ConfirmVoteResult type="button">결과 확인</ConfirmVoteResult>
       </ConfirmVoteBtns>
     </ConfirmVote>
@@ -62,7 +87,7 @@ const ConfirmVoteData = styled.div`
   }
 `;
 
-const ConfirmVoteList = styled.button`
+const ConfirmVoteList = styled.button<{ $isActive: boolean }>`
   position: relative;
   overflow: hidden;
   margin-top: 12px;
@@ -76,10 +101,16 @@ const ConfirmVoteList = styled.button`
   border: 1px solid ${({ theme }) => theme.colors.gray8};
   border-radius: ${({ theme }) => theme.borderRadius.borderRadius10};
   z-index: 3;
+  color: ${({ $isActive, theme }) => ($isActive ? theme.colors.gray2 : '')};
+  background-color: ${({ $isActive, theme }) =>
+    $isActive ? theme.colors.gray8 : ''};
 
-  /* active
-  color: var(--gray2);
-	background-color: var(--gray8); */
+  & svg {
+    display: ${({ $isActive }) => ($isActive ? '' : 'none')};
+    width: ${({ $isActive }) => ($isActive ? '20px' : '')};
+    height: ${({ $isActive }) => ($isActive ? '20px' : '')};
+    margin-right: ${({ $isActive }) => ($isActive ? '10px' : '')};
+  }
 `;
 
 const ConfirmVoteBtns = styled.div`
@@ -98,9 +129,10 @@ const ConfirmVoteSubmit = styled.button`
   color: ${({ theme }) => theme.colors.gray8};
   cursor: initial;
 
-  /* disabled 
-  color: var(--gray5); 
-	cursor: not-allowed; */
+  &:disabled {
+    color: ${({ theme }) => theme.colors.gray5};
+    cursor: not-allowed;
+  }
 `;
 
 const ConfirmVoteResult = styled.button`
