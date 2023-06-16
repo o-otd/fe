36abfff859b08818a73ae178f546339190ea6c7f
jungyu333 <jungyu3826@naver.com>
2023-06-16 21:02:18 +0900

    [feat] confirm list get api 적용 & ConfirmContentCard data binding

---
+++ src/components/Home/Confirm/ConfirmListItem.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import ConfirmContentCard from './ConfirmContentCard';
+import ConfirmVoteCard from './ConfirmVoteCard';
+import ConfirmImageSlide from './ConfirmImageSlide';
+import ConfirmComment from './ConfirmComment';
+import { IConfirmListItemProps } from 'types/Home/Confirm';
+
+function ConfirmListItem({ confirmData }: IConfirmListItemProps) {
+  return (
+    <Wrapper>
+      <ConfirmContentCard
+        user={confirmData.user}
+        startDate={confirmData.startDate}
+        endDate={confirmData.endDate}
+        content={confirmData.content}
+      />
+
+      <ConfirmVoteCard />
+
+      <ConfirmImageSlide />
+
+      <ConfirmComment />
+    </Wrapper>
+  );
+}
+
+export default ConfirmListItem;
+
+const Wrapper = styled.li`
+  & + & {
+    margin-top: 24px;
+  }
+`;
--- src/page/confirm/Confirm.tsx
+++ src/page/confirm/Confirm.tsx
@@ -1,23 +1,27 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
 import ConfirmHeader from 'components/Home/Confirm/ConfirmHeader';
-import ConfirmContentCard from 'components/Home/Confirm/ConfirmContentCard';
-import ConfirmVoteCard from 'components/Home/Confirm/ConfirmVoteCard';
-import ConfirmImageSlide from 'components/Home/Confirm/ConfirmImageSlide';
-import ConfirmComment from 'components/Home/Confirm/ConfirmComment';
 import { useApi } from 'hooks/useApi';
 import { getConfirms } from 'api/confirm';
+import { IGetConfirmsApiDataResponse } from 'types/Home/Confirm';
+import ConfirmListItem from 'components/Home/Confirm/ConfirmListItem';
 
 function Confirm() {
+  const [confirms, setConfirms] = useState<IGetConfirmsApiDataResponse[]>([]);
   const { execute, error } = useApi(getConfirms);
+
   useEffect(() => {
     const fetchConfirms = async () => {
       const response = await execute();
-      console.log(response);
+      if (response) {
+        setConfirms(response.data.datas);
+      } else {
+        return null;
+      }
     };
 
-    //fetchConfirms();
+    fetchConfirms();
   }, [execute]);
+
   return (
     <>
       <main>
@@ -25,15 +29,10 @@ function Confirm() {
 
         <section>
           <ul>
-            <ConfirmListItem>
-              <ConfirmContentCard />
-
-              <ConfirmVoteCard />
-
-              <ConfirmImageSlide />
-
-              <ConfirmComment />
-            </ConfirmListItem>
+            {confirms &&
+              confirms.map((confirm) => (
+                <ConfirmListItem confirmData={confirm} key={confirm.id} />
+              ))}
           </ul>
         </section>
       </main>
@@ -42,9 +41,3 @@ function Confirm() {
 }
 
 export default Confirm;
-
-const ConfirmListItem = styled.li`
-  & + & {
-    margin-top: 24px;
-  }
-`;
