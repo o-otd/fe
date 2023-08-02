9141ea02da8f8584ca82a72be4575049835bafee
jungyu333 <jungyu3826@naver.com>
2023-08-02 21:27:34 +0900

    [feat] CommentDropBox layout 구현

---
+++ src/components/Home/Confirm/CommentDetail/CommentDropBox.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function CommentDropBox() {
+  return (
+    <CommentsMoreList>
+      {/* <CommentsMoreItem>답댓글</CommentsMoreItem> */}
+      <CommentsMoreItem>수정</CommentsMoreItem>
+      <CommentsMoreItemDelete>삭제</CommentsMoreItemDelete>
+    </CommentsMoreList>
+  );
+}
+
+export default CommentDropBox;
+
+const CommentsMoreList = styled.ul`
+  position: absolute;
+  cursor: pointer;
+  top: 20px;
+  right: 0;
+  width: 81px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadius5};
+`;
+
+const CommentsMoreItem = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+  color: ${({ theme }) => theme.colors.gray1};
+  font-weight: 500;
+  font-size: 13px;
+`;
+
+const CommentsMoreItemDelete = styled(CommentsMoreItem)`
+  color: ${({ theme }) => theme.colors.tint1};
+`;
--- src/components/Home/Confirm/CommentDetail/index.ts
+++ src/components/Home/Confirm/CommentDetail/index.ts
@@ -4,3 +4,4 @@ export { default as CommentItem } from './CommentItem';
 export { default as CommentsInput } from './CommentsInput';
 export { default as CommentsItem } from './CommentsItem';
 export { default as NestedComments } from './NestedComments';
+export { default as CommentDropBox } from './CommentDropBox';
