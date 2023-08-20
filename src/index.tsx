163cc6bf8864cf3e94cadf1b39eedb065bf2de1a
jungyu333 <jungyu3826@naver.com>
2023-08-20 14:36:21 +0900

    [feat] react-query setting

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,25 @@ import { theme } from 'styles/theme';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <GlobalFonts />
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+        </QueryClientProvider>
       </ThemeProvider>
     </Provider>
   </>,
